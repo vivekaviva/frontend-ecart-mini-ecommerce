33cb8e1ab5a988e050251839259d37fcaa971ea0
vivekaviva <vivekaloganathan37@gmail.com>
2024-11-13 12:04:29 +0530

    with api call

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,31 @@
 import { Fragment, useState, useEffect } from "react";
-import ProductCard from "../components/ProductCard";
 
 export default function Home() {
   const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    console.log("Environment Variables:", process.env);
+    // const apiUrl = process.env.REACT_APP_API_URL || "Not Defined";
+    const apiUrl = "http://localhost:8000";
+    console.log("API URL:", apiUrl);
+
+    const url = apiUrl + "/products";
+    console.log("URL:", url);
+
+    fetch(url)
+      .then((res) => {
+        if (!res) {
+          throw new Error("Internal Server error");
+        }
+        return res.json();
+      })
+      .then((res) => {
+        console.log("response from api", res.data);
+        setProducts(res.data);
+      })
+      .catch((err) => console.log("Error", err));
+  }, []);
+  console.log("products", products);
   return (
     <Fragment>
       <div className="container">
